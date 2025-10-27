1f770043df852a26352bca8c468b50bb893ad817
riteshsingh3628 <riteshsingh3628@gmail.com>
2025-10-27 10:38:38 +0530

    backend: login feature

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,12 +6,24 @@ export const register = async(req,res)=>{
         if(result?.status === 'error'){
             return errorResponse(res,400,result.message);
         }
-        console.log(result.data)
         return successResponse(res,result.data,'User registered successfully',201);
         
     } catch (error) {
-        
+        console.log("register controller error:",error);
     }
 }
 
+export const login = async(req,res)=>{
+    try {
+        const result = await authService.login(req.body);
+        if(result?.status === 'error'){
+            return errorResponse(res,400,result.message);
+        }
+        return successResponse(res,result.data,'User login successful',200);
+
+    } catch (error) {
+        console.log("login controller error:",error);
+        
+    }
+}
 export default {register};
